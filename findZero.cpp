81ff6c0a345f47605358a9c1ffc247b722a91f55
XiaoHuanzzz <140334171+XiaoHuanzzz@users.noreply.github.com>
2023-07-24 17:30:59 +0800

    Update findZero.cpp

--- findZero.cpp
+++ findZero.cpp
@@ -2,20 +2,22 @@
  * @Author: (xiao_huan) 1156492597@qq.com
  * @Date: 2023-07-19 16:08:58
  * @LastEditors: (xiao_huan) 1156492597@qq.com
- * @LastEditTime: 2023-07-22 18:50:33
+ * @LastEditTime: 2023-07-24 17:07:11
  * @FilePath: \Numerical Analysis Exercise\findZero_InRange.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include <iostream>
 #include <cmath>
 #include <functional>
+#include <vector>
+#include <limits>
 
 #include "functions.h"
 
 class FunctionSolver {
 private:
-    double a_;  // 存储区间左端点
-    double b_;  // 存储区间右端点
+    double a_ = 1.0;  // 存储区间左端点
+    double b_ = 3.0;  // 存储区间右端点
     double epsilon_;  // 零点的精度
 
 public:
@@ -62,9 +64,9 @@ public:
         return (left + right) / 2.0;
     }
 
-    double findZeroNewton(std::function<double(double)> func) {
+    double findZeroNewton(std::function<double(double)> func, double x0 = 0) {
     const double epsilon = 1e-6;  // 零点的精度
-    double x = 0.0;  // 初始猜测值
+    double x = x0;  // 初始猜测值
 
     while (std::abs(func(x)) > epsilon) {
         // 使用牛顿法进行迭代求解
@@ -75,7 +77,7 @@ public:
     return x;
 }
 
-    double findDerivativeZeros(const std::function<double(double)>& func, double initial_guess, double epsilon = 1e-6, int max_iterations = 100) {
+    double findDerivativeZeroNewton(const std::function<double(double)>& func, double initial_guess, double epsilon = 1e-12, int max_iterations = 100) {
         double x = initial_guess;
         int iteration = 0;
 
@@ -101,6 +103,66 @@ public:
 
         std::cout << "can't find the point in " << max_iterations << " steps " << std::endl;
         return initial_guess; // 未找到满足条件的点，返回初始猜测值
+    }
+
+    std::vector<double> findDerivativeZeros(const std::function<double(double)>& func, double epsilon = 1e-6) {
+        std::vector<double> zeros;
+
+        double x = 0.0;
+        double stepSize = 1.0;
+        double rightX = x, leftX = x;
+        double rrightX, lleftX;
+
+        // double maxDouble = std::numeric_limits<double>::max();
+
+        while (true) {
+            rrightX = rightX + stepSize;
+            double rightDerivative = calculateDerivative(func, rightX);
+            double rrightDerivative = calculateDerivative(func, rrightX);
+            lleftX = leftX - stepSize;
+            double leftDerivative = calculateDerivative(func, leftX);
+            double lleftDerivative = calculateDerivative(func, lleftX);
+
+            if (rightDerivative * rrightDerivative <= 0) {
+                // If derivative values have different signs, perform binary search
+                double left = rightX;
+                double right = rrightX;
+                while (right - left > 1e-6) {
+                    double mid = (left + right) / 2;
+                    double midDerivative = calculateDerivative(func, mid);
+                    if (midDerivative * calculateDerivative(func, left) <= 0) {
+                        right = mid;
+                    } else {
+                        left = mid;
+                    }
+                }
+                zeros.push_back((left + right) / 2);
+            }
+
+            if (leftDerivative * lleftDerivative <= 0) {
+                // If derivative values have different signs, perform binary search
+                double left = lleftX;
+                double right = leftX;
+                while (right - left > 1e-6) {
+                    double mid = (left + right) / 2;
+                    double midDerivative = calculateDerivative(func, mid);
+                    if (midDerivative * calculateDerivative(func, left) <= 0) {
+                        right = mid;
+                    } else {
+                        left = mid;
+                    }
+                }
+                zeros.push_back((left + right) / 2);
+            }
+            rightX = rrightX;
+            leftX = lleftX;
+        
+            if(rightX >= 1.0/epsilon){
+                break;
+            }
+            stepSize *= 2.0;
+        }
+        return zeros;
     }
 
     void findZeroInRange(int method) {
@@ -144,8 +206,26 @@ public:
         }; // Amazing!
         size_t numFunctions = sizeof(functions) / sizeof(functions[0]);
         for (size_t i = 0; i < numFunctions; ++i) {
-            double zero = findDerivativeZeros(functions[i],0.0);
-            std::cout << "Zero of func" << i+1 << ": " << zero << std::endl;
+            double zero = findDerivativeZeroNewton(functions[i],0.0);
+        }
+    }
+
+    void findDerivativeZerosInRange()
+    {
+         std::function<double(double)> functions[] = {
+            [this](double x) { return func1(x); },
+            [this](double x) { return func2(x); },
+            [this](double x) { return func3(x); },
+            [this](double x) { return func4(x); }
+        }; // Amazing!
+        size_t numFunctions = sizeof(functions) / sizeof(functions[0]);
+        for (size_t i = 0; i < numFunctions; ++i) {
+            std::vector<double> zeros = findDerivativeZeros(functions[i]);
+            std::cout << "Derivate zeros of func" << i+1 << ": " << std::endl;
+            for (size_t i = 0; i < zeros.size(); ++i) {
+                std::cout << zeros[i] << " ";
+            }
+            std::cout << std::endl;
         }
     }
 };
@@ -158,6 +238,6 @@ int main() {
     FunctionSolver solver(a, b, epsilon);
     solver.findZeroInRange(1); 
     solver.findDerivativeZeroInRange();
-
+    solver.findDerivativeZerosInRange();
     return 0;
 }
