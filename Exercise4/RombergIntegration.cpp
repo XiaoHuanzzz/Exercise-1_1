a6963e29c4a36a669d794a5b92ccbe465b584d50
XiaoHuanzzz <140334171+XiaoHuanzzz@users.noreply.github.com>
2023-10-06 14:52:58 +0800

    Update RombergIntegration.cpp

--- Exercise4/RombergIntegration.cpp
+++ Exercise4/RombergIntegration.cpp
@@ -2,7 +2,7 @@
  * @Author: (xiao_huan) 1156492597@qq.com
  * @Date: 2023-10-05 14:02:44
  * @LastEditors: (xiao_huan) 1156492597@qq.com
- * @LastEditTime: 2023-10-06 14:41:17
+ * @LastEditTime: 2023-10-06 14:51:57
  * @FilePath: \Exercise4\RombergIntegration.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -24,16 +24,17 @@ vector<vector<double>> RombergIntegration(double a, double b, function<double(do
     vector<vector<double>> R(3, vector<double>(n+1, 0.0));
     double h = b-a;
     R[1][1] = (func(a)+func(b))*h/2;
+    cout << "R_1,1 = " << R[1][1] << endl;
     for(int i = 2; i <= n; i++){
         R[2][1] = R[1][1];
         for(int k = 0; k < pow(2, i-2); k++){
             R[2][1] += h*func(a+(k-0.5)*h);
         }
         R[2][1] /= 2;
-        cout << "R_2,1 = " << R[2][1] << ", ";
+        cout << "R_" << i << ",1 = " << R[2][1] << ", ";
         for(int j = 2; j <= i; j++){
             R[2][j] = R[2][j-1] + (R[2][j-1]-R[1][j-1])/(pow(4,j-1)-1);
-            cout << "R_2," << j << " = " << R[2][j] << ", ";
+            cout << "R_" << i << "," << j << " = " << R[2][j] << ", ";
         }
         cout << endl;
         h /= 2;
